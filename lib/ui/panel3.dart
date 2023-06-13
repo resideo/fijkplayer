72ee5951ed6550fef9b04dae2479504433f553b1
Chethana B L <chethana.bl@resideo.com>
2023-06-13 15:42:30 +0530

    feat: Updating panel builder for live stream ended flow Removed unused methods.

--- lib/ui/panel3.dart
+++ lib/ui/panel3.dart
@@ -25,7 +25,6 @@ part of fijkplayer;
 FijkPanelWidgetBuilder fijkPanel3Builder({
   Key? key,
   final bool fill = false,
-  final int duration = 4000,
   final bool doubleTap = true,
   final bool snapShot = false,
   final VoidCallback? onBack,
@@ -34,6 +33,11 @@ FijkPanelWidgetBuilder fijkPanel3Builder({
   final Widget? pushToTalkIcon,
   final Widget? recordIcon,
   final Widget? snapshotIcon,
+  final bool shouldShowLoader = false,
+  final Widget? loaderView,
+  final bool hasLiveStreamEnded = false,
+  final Widget? liveStreamEndedView,
+  final Widget? liveStreamErrorView,
 }) {
   return (FijkPlayer player, FijkData data, BuildContext context, Size viewSize,
       Rect texturePos) {
@@ -47,12 +51,16 @@ FijkPanelWidgetBuilder fijkPanel3Builder({
       recordIcon: recordIcon,
       snapshotIcon: snapshotIcon,
       pushToTalkIcon: pushToTalkIcon,
+      shouldShowLoader: shouldShowLoader,
+      loaderView: loaderView,
+      hasLiveStreamEnded: hasLiveStreamEnded,
+      liveStreamEndedView: liveStreamEndedView,
+      liveStreamErrorView: liveStreamErrorView,
       viewSize: viewSize,
       texPos: texturePos,
       fill: fill,
       doubleTap: doubleTap,
       snapShot: snapShot,
-      hideDuration: duration,
     );
   };
 }
@@ -66,12 +74,16 @@ class _FijkPanel3 extends StatefulWidget {
   final Widget? pushToTalkIcon;
   final Widget? recordIcon;
   final Widget? snapshotIcon;
+  final bool shouldShowLoader;
+  final Widget? loaderView;
+  final bool hasLiveStreamEnded;
+  final Widget? liveStreamEndedView;
+  final Widget? liveStreamErrorView;
   final Size viewSize;
   final Rect texPos;
   final bool fill;
   final bool doubleTap;
   final bool snapShot;
-  final int hideDuration;
 
   const _FijkPanel3(
       {Key? key,
@@ -84,13 +96,16 @@ class _FijkPanel3 extends StatefulWidget {
       this.pushToTalkIcon,
       this.recordIcon,
       this.snapshotIcon,
+      this.shouldShowLoader = false,
+      this.loaderView,
+      this.hasLiveStreamEnded = false,
+      this.liveStreamEndedView,
+      this.liveStreamErrorView,
       required this.viewSize,
-      this.hideDuration = 4000,
       this.doubleTap = false,
       this.snapShot = false,
       required this.texPos})
-      : assert(hideDuration > 0 && hideDuration < 10000),
-        super(key: key);
+      : super(key: key);
 
   @override
   __FijkPanel3State createState() => __FijkPanel3State();
@@ -99,11 +114,8 @@ class _FijkPanel3 extends StatefulWidget {
 class __FijkPanel3State extends State<_FijkPanel3> {
   FijkPlayer get player => widget.player;
 
-  Timer? _statelessTimer;
   bool _prepared = false;
   bool _isPlaying = false;
-
-  Duration _duration = Duration();
 
   StreamSubscription? _currentPosSubs;
   StreamSubscription? _bufferPosSubs;
@@ -120,7 +132,6 @@ class __FijkPanel3State extends State<_FijkPanel3> {
     _valController = StreamController.broadcast();
     _prepared = player.state.index >= FijkState.prepared.index;
     _isPlaying = player.state == FijkState.started;
-    _duration = player.value.duration;
 
     _currentPosSubs = player.onCurrentPosUpdate.listen((v) {
       if (_needClearSeekData) {
@@ -138,7 +149,6 @@ class __FijkPanel3State extends State<_FijkPanel3> {
   void dispose() {
     super.dispose();
     _valController.close();
-    _statelessTimer?.cancel();
     _currentPosSubs?.cancel();
     _bufferPosSubs?.cancel();
     player.removeListener(_playerValueChanged);
@@ -146,12 +156,9 @@ class __FijkPanel3State extends State<_FijkPanel3> {
 
   void _playerValueChanged() {
     FijkValue value = player.value;
-
-    if (value.duration != _duration) {
-      _duration = value.duration;
-    }
     bool playing = (value.state == FijkState.started);
     bool prepared = value.prepared;
+
     if (playing != _isPlaying ||
         prepared != _prepared ||
         value.state == FijkState.asyncPreparing) {
@@ -162,98 +169,36 @@ class __FijkPanel3State extends State<_FijkPanel3> {
     }
   }
 
-  Widget _buildFullScreenButton() {
-    Icon icon = player.value.fullScreen
-        ? Icon(Icons.fullscreen_exit)
-        : Icon(Icons.fullscreen);
-
-    return IconButton(
-      padding: EdgeInsets.all(0),
-      color: Colors.white,
-      icon: icon,
-      onPressed: () {
-        player.value.fullScreen
-            ? player.exitFullScreen()
-            : player.enterFullScreen();
-      },
-    );
-  }
-
-  Widget _buildBottom() {
-    return Row(
-      children: <Widget>[
-        Expanded(child: Container()),
-        _buildFullScreenButton(),
-      ],
-    );
-  }
-
   Widget _buildPanel(BuildContext context) {
-    double height = _panelHeight();
-
     bool fullScreen = player.value.fullScreen;
-    Widget centerWidget = Container(
-      color: Color(0x00000000),
-    );
 
-    if (fullScreen) {
-      centerWidget = Row(
-        children: <Widget>[
-          Expanded(child: Container()),
-          Padding(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 8, bottom: 8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                if (widget.pushToTalkIcon != null) widget.pushToTalkIcon!,
-                if (widget.recordIcon != null) widget.recordIcon!,
-                if (widget.snapshotIcon != null) widget.snapshotIcon!,
-                if (widget.alarmIcon != null) widget.alarmIcon!,
-              ],
-            ),
-          )
-        ],
+    if (widget.shouldShowLoader) {
+      return _buildLoader();
+    } else if (widget.hasLiveStreamEnded) {
+      return _buildCustomWidgetsFor(
+        view: widget.liveStreamEndedView != null
+            ? widget.liveStreamEndedView!
+            : SizedBox(),
       );
-    }
-
-    if (fullScreen) {
-      return Container(
-        padding: EdgeInsets.all(15),
-        child: Row(
+    } else if (_isPlaying) {
+      if (fullScreen) {
+        return Stack(
           children: [
-            Column(
-              children: [
-                _roundIconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: () => player.exitFullScreen(),
-                ),
-                Expanded(child: Container()),
-              ],
-            ),
-            Expanded(child: centerWidget),
+            _buildCloseButton(),
+            _buildFullScreenControls(),
           ],
-        ),
-      );
+        );
+      } else {
+        return Stack(
+          children: [
+            if (_isPlaying && widget.liveLabel != null)
+              Positioned(left: 10, bottom: 10, child: widget.liveLabel!),
+            _buildFullScreenButton(),
+          ],
+        );
+      }
     } else {
-      return Stack(
-        children: [
-          if (_isPlaying && widget.liveLabel != null)
-            Positioned(
-              left: 10,
-              bottom: 10,
-              child: widget.liveLabel!,
-            ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: height > 80 ? 45 : height / 2,
-              padding: EdgeInsets.only(left: 8, right: 8, bottom: 5),
-              child: _buildBottom(),
-            ),
-          ),
-        ],
-      );
+      return Container();
     }
   }
 
@@ -268,54 +213,95 @@ class __FijkPanel3State extends State<_FijkPanel3> {
     return rect;
   }
 
-  double _panelHeight() {
-    if (player.value.fullScreen || (true == widget.fill)) {
-      return widget.viewSize.height;
+  Widget _buildCloseButton({bool isDarkTheme = true}) {
+    return Positioned(
+      left: 10,
+      top: 10,
+      child: _roundIconButton(
+        icon: Icon(
+          Icons.close,
+          color: isDarkTheme ? Colors.white : Colors.black,
+        ),
+        onPressed: () => player.exitFullScreen(),
+        isDarkTheme: isDarkTheme,
+      ),
+    );
+  }
+
+  Widget _buildFullScreenButton() {
+    Icon icon = Icon(Icons.fullscreen);
+
+    return Positioned(
+      right: 8,
+      bottom: 4,
+      child: IconButton(
+        padding: EdgeInsets.all(0),
+        color: Colors.white,
+        icon: icon,
+        onPressed: () => player.enterFullScreen(),
+      ),
+    );
+  }
+
+  Widget _buildFullScreenControls() {
+    return Positioned(
+      right: 20,
+      bottom: 20,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          if (widget.pushToTalkIcon != null) widget.pushToTalkIcon!,
+          if (widget.recordIcon != null) widget.recordIcon!,
+          if (widget.snapshotIcon != null) widget.snapshotIcon!,
+          if (widget.alarmIcon != null) widget.alarmIcon!,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoader() {
+    return (widget.loaderView != null)
+        ? widget.loaderView!
+        : Container(
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(
+                Color(0xFF7DAAF7),
+              ),
+            ),
+          );
+  }
+
+  Widget _buildCustomWidgetsFor({required Widget view}) {
+    bool fullScreen = player.value.fullScreen;
+
+    if (fullScreen) {
+      return Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: view,
+          ),
+          _buildCloseButton(isDarkTheme: false)
+        ],
+      );
     } else {
-      return min(widget.viewSize.height, widget.texPos.bottom) -
-          max(0.0, widget.texPos.top);
+      return Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: view,
+          ),
+          _buildFullScreenButton()
+        ],
+      );
     }
   }
 
-  Widget _buildBack(BuildContext context) {
-    return IconButton(
-      padding: EdgeInsets.only(left: 5),
-      icon: Icon(
-        Icons.arrow_back_ios,
-        color: Color(0xDDFFFFFF),
-      ),
-      onPressed: widget.onBack,
-    );
-  }
-
-  Widget _roundIconButton(
-      {required Widget icon, required VoidCallback? onPressed}) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 2),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          shape: const CircleBorder(),
-          shadowColor: Colors.black,
-          backgroundColor: Colors.black.withOpacity(0.4),
-          padding: const EdgeInsets.all(16),
-        ),
-        onPressed: onPressed,
-        child: icon,
-      ),
-    );
-  }
-
-  Widget _buildStateless() {
-    if (player.state == FijkState.asyncPreparing) {
-      return Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation(
-            Color(0xFF7DAAF7),
-          ),
-        ),
-      );
-    } else if (player.state == FijkState.error) {
+  Widget _buildErrorWidget() {
+    if (widget.liveStreamErrorView != null) {
+      return _buildCustomWidgetsFor(view: widget.liveStreamErrorView!);
+    } else {
       return Container(
         alignment: Alignment.center,
         child: Icon(
@@ -324,9 +310,31 @@ class __FijkPanel3State extends State<_FijkPanel3> {
           color: Color(0x99FFFFFF),
         ),
       );
-    } else {
-      return Container();
     }
+  }
+
+  Widget _roundIconButton({
+    required Widget icon,
+    required VoidCallback? onPressed,
+    bool isDarkTheme = true,
+  }) {
+    final shadowColor = isDarkTheme ? Colors.black : Colors.white;
+    final backgroundColor =
+        isDarkTheme ? Colors.black.withOpacity(0.4) : Colors.white;
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 2),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          shadowColor: shadowColor,
+          backgroundColor: backgroundColor,
+          padding: const EdgeInsets.all(16),
+        ),
+        onPressed: onPressed,
+        child: icon,
+      ),
+    );
   }
 
   @override
@@ -335,19 +343,14 @@ class __FijkPanel3State extends State<_FijkPanel3> {
 
     List ws = <Widget>[];
 
-    if (_statelessTimer != null && _statelessTimer!.isActive) {
-      ws.add(_buildStateless());
-    } else if (player.state == FijkState.asyncPreparing) {
-      ws.add(_buildStateless());
+    if (player.state == FijkState.asyncPreparing) {
+      ws.add(_buildLoader());
     } else if (player.state == FijkState.error) {
-      ws.add(_buildStateless());
+      ws.add(_buildErrorWidget());
     }
 
     ws.add(_buildPanel(context));
 
-    if (widget.onBack != null) {
-      ws.add(_buildBack(context));
-    }
     return Positioned.fromRect(
       rect: rect,
       child: Stack(children: ws as List<Widget>),
